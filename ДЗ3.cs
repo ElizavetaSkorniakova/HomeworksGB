b93841151ceade195d7fa2920f724eb97ded74b7
Elizaveta Skorniakova <120256320+ElizavetaSkorniakova@users.noreply.github.com>
2023-01-06 20:25:19 +0300

    Update ДЗ3.cs

--- ДЗ3.cs
+++ ДЗ3.cs
@@ -1,10 +1,3 @@
-﻿// int getUserValue(string message)
-// {
-//     Console.WriteLine(message);
-//     int result=int.Parse(Console.ReadLine()!);
-//     return result;
-// }
-
 
 // Задача 19. Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
 string a;
